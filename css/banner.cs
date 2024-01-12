a87d4c265347eedc7bf793ce531b41b71a1a38d4
anju <anjuk@spericorn.com>
2024-01-12 19:45:46 +0530

    homepage

---
+++ css/banner.cs
@@ -0,0 +1,103 @@
+/*** all Mobile Screen (iphone 6,7,8) ***/
+/*** mobile Landscape Size ***/
+/*** medium devices (tablets, less than 992px) ***/
+/*** iPad in landscape ***/
+/**** smaller than desktop 1200 (devices and browsers) ***/
+/*** laptop with MDPI (medium density per inch) resolution ***/
+/*** SP developers resolution ***/
+/*** laptop with HiDPI (High Dots Per Inch) resolution ***/
+/*** WSXGA (Wide Super Extended Graphics Array) resolution ***/
+/* placeholder mixin */
+.banner {
+  background: url("/img/Banner.jpg") no-repeat top right;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+}
+.banner .bnr-left {
+  max-width: 745px;
+  margin-right: 138px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+}
+@media (max-width: 767px) {
+  .banner .bnr-left {
+    margin-right: 0px;
+    max-width: 100%;
+  }
+}
+.banner .bnr-left .bnr-title {
+  font: normal normal 700 44px/67px "Titillium Web", sans-serif;
+  margin-bottom: 13px;
+}
+@media (max-width: 1679px) {
+  .banner .bnr-left .bnr-title {
+    font: normal normal 700 40px/60px "Titillium Web", sans-serif;
+  }
+}
+@media (max-width: 1481px) {
+  .banner .bnr-left .bnr-title {
+    font: normal normal 700 38px/53px "Titillium Web", sans-serif;
+  }
+}
+@media (max-width: 1199px) {
+  .banner .bnr-left .bnr-title {
+    font: normal normal 700 35px/50px "Titillium Web", sans-serif;
+  }
+}
+@media (max-width: 991px) {
+  .banner .bnr-left .bnr-title {
+    font: normal normal 700 30px/40px "Titillium Web", sans-serif;
+  }
+}
+@media (max-width: 767px) {
+  .banner .bnr-left .bnr-title {
+    font: normal normal 700 28px/38px "Titillium Web", sans-serif;
+  }
+}
+@media (max-width: 579px) {
+  .banner .bnr-left .bnr-title {
+    font: normal normal 700 25px/35px "Titillium Web", sans-serif;
+  }
+}
+.banner .bnr-left .bnr-title span {
+  display: block;
+  font: normal normal 300 44px/67px "Titillium Web", sans-serif;
+}
+@media (max-width: 1679px) {
+  .banner .bnr-left .bnr-title span {
+    font: normal normal 300 40px/60px "Titillium Web", sans-serif;
+  }
+}
+@media (max-width: 1481px) {
+  .banner .bnr-left .bnr-title span {
+    font: normal normal 300 38px/53px "Titillium Web", sans-serif;
+  }
+}
+@media (max-width: 1199px) {
+  .banner .bnr-left .bnr-title span {
+    font: normal normal 300 35px/55px "Titillium Web", sans-serif;
+  }
+}
+@media (max-width: 991px) {
+  .banner .bnr-left .bnr-title span {
+    font: normal normal 300 30px/40px "Titillium Web", sans-serif;
+  }
+}
+@media (max-width: 767px) {
+  .banner .bnr-left .bnr-title span {
+    font: normal normal 300 28px/38px "Titillium Web", sans-serif;
+  }
+}
+@media (max-width: 579px) {
+  .banner .bnr-left .bnr-title span {
+    font: normal normal 300 25px/35px "Titillium Web", sans-serif;
+  }
+}
+
+.main-desc1 {
+  color: #fff !important;
+}
+
+/*# sourceMappingURL=banner.cs.map */
