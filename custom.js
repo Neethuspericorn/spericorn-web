54f51669231db253e93958c3f541117c2a830381
Niyas MN <niyas.mn@spericorn.com>
2024-01-12 19:05:55 +0530

    first commit

---
+++ custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $.scrollify({
+        section : ".slide",
+        scrollSpeed: 200,
+        setHeights: true,
+        offset: '',
+        sectionName:false,
+        interstitialSection:"header,footer"
+    });
+    $(".navbar-toggler").click(function() {
+        $(this).toggleClass("on");
+        $(".navbar-collapse").slideToggle();
+      });
+});
