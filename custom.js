47f0fc55ce766d6071e5623c1bc8e7e316454ae1
Antony spericorn <antony@spericorn.com>
2024-01-15 13:16:39 +0530

    common.scss update

--- custom.js
+++ custom.js
@@ -11,4 +11,12 @@ $(document).ready(function(){
         $(this).toggleClass("on");
         $(".navbar-collapse").slideToggle();
       });
+
+      $('.testi-slider').slick({
+            arrows: false,
+            dots: true,
+            prevArrow: $('.slick-arrow-wrp .btn-prev'),
+            nextArrow: $('.slick-arrow-wrp .btn-next')
+      });
+
 });
