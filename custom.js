86ac938a5ca88f9cbc7350e3d321d2d74fc5a8ca
Niyas MN <niyas.mn@spericorn.com>
2024-01-15 12:58:35 +0530

    tab updated

--- custom.js
+++ custom.js
@@ -10,5 +10,6 @@ $(document).ready(function(){
     $(".navbar-toggler").click(function() {
         $(this).toggleClass("on");
         $(".navbar-collapse").slideToggle();
-      });
+    });
+    
 });
