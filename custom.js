4c8d79851df6d93d1d13c52032b499af153f9dc5
Antony spericorn <antony@spericorn.com>
2024-01-16 18:07:19 +0530

    scroll to section

--- custom.js
+++ custom.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $.scrollify({
-        section : ".slide",
-        scrollSpeed: 200,
-        setHeights: true,
-        offset: 0,
-        sectionName:false,
-        interstitialSection:"header,footer"
-    });
+    // $.scrollify({
+    //     section : ".slide",
+    //     scrollSpeed: 200,
+    //     setHeights: true,
+    //     offset: 0,
+    //     sectionName:false,
+    //     interstitialSection:"header,footer"
+    // });
     $(".menu-toggler").click(function() {
         $(this).toggleClass("on");
         $(".menu-collapse").slideToggle();
@@ -19,7 +19,81 @@ $(document).ready(function(){
             nextArrow: '#testi-next'
       });
 
-    
+    // scrolling scripts
+
+    var selector = ".scroll-panel";
+
+    var $slides = $(selector);
+
+    var currentSlide = 0;
+    var isAnimating = false;
+
+    var stopAnimation = function () {
+      setTimeout(function () {
+        isAnimating = false;
+      }, 300);
+    };
+
+    var bottomIsReached = function ($elem) {
+      var rect = $elem[0].getBoundingClientRect();
+      return rect.bottom <= $(window).height();
+    };
+
+    var topIsReached = function ($elem) {
+      var rect = $elem[0].getBoundingClientRect();
+      return rect.top >= 0;
+    };
+
+    document.addEventListener(
+      "wheel",
+      function (event) {
+        var $currentSlide = $($slides[currentSlide]);
+
+        if (isAnimating) {
+          event.preventDefault();
+          return;
+        }
+
+        var direction = -event.deltaY;
+
+        if (direction < 0) {
+          // next
+          if (currentSlide + 1 >= $slides.length) return;
+          if (!bottomIsReached($currentSlide)) return;
+          event.preventDefault();
+          currentSlide++;
+          var $slide = $($slides[currentSlide]);
+          var offsetTop = $slide.offset().top;
+          isAnimating = true;
+          $("html, body").animate(
+            {
+              scrollTop: offsetTop - 60
+            },
+            1000,
+            stopAnimation
+          );
+        } else {
+          // back
+          if (currentSlide - 1 < 0) return;
+          if (!topIsReached($currentSlide)) return;
+          event.preventDefault();
+          currentSlide--;
+          var $slide = $($slides[currentSlide]);
+          var offsetTop = $slide.offset().top;
+          isAnimating = true;
+          $("html, body").animate(
+            {
+              scrollTop: offsetTop
+            },
+            1000,
+            stopAnimation
+          );
+        }
+      },
+      { passive: false }
+    );
+
+    // scrolling scripts end here
 });
 
 function openService(evt, serviceName) {
@@ -53,6 +127,16 @@ function openService(evt, serviceName) {
 }
 // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpener").click();
+
+
+
+
+  $(document).ready(function () {
+
+    
+
+  });
+
 
 
 
