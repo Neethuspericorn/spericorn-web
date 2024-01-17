a65183fac99164025f74d456dfdd048b36aa2eda
Niyas MN <niyas.mn@spericorn.com>
2024-01-17 13:49:00 +0530

    slide updates

--- custom.js
+++ custom.js
@@ -139,26 +139,126 @@ function openService(evt, serviceName) {
 // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpener").click();
 
+// accordion code start here
 
+        var accItem = document.getElementsByClassName("acco-head"),
+        accBody = document.getElementsByClassName("acco-body");
+        for (i = 0; i < accItem.length; i++) accItem[i].onclick = function() {
+            var e = !this.classList.contains("active");
+            setClass(accItem, "active", "remove"), setClass(accBody, "show", "remove"), e && (this.classList.toggle("active"), this.nextElementSibling.classList.toggle("show"), this.nextElementSibling.clientHeight)
+        };
+
+// var acc = document.getElementsByClassName("acco-head");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+function setClass(e, t, s) {
+  for (var a = 0; a < e.length; a++) e[a].classList[s](t)
+}
+// accordion code end here
 
 
   $(document).ready(function () {
 
-    $(".tab-slide").slick({
+    $(".bnr-icon-slide").slick({
       mobileFirst: true, 
-      slide: 'li',
       arrows: false,
-      slidesToShow: 3,
+      slidesToShow: 4,
       slidesToScroll: 1,
       responsive: [
         {
-            breakpoint: 767,
+            breakpoint: 991,
             settings: "unslick",
         }
       ]
     })
 
+    // $(".tab-slide").slick({
+    //   mobileFirst: true, 
+    //   slide: 'li',
+    //   arrows: false,
+    //   slidesToShow: 3,
+    //   slidesToScroll: 1,
+    //   responsive: [
+    //     {
+    //         breakpoint: 767,
+    //         settings: "unslick",
+    //     }
+    //   ]
+    // })
+
+    function initSlider() {
+      return $('.tab-slide').slick({
+        mobileFirst: true, 
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: "unslick"
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              arrows: true,
+            }
+          },
+          {
+            breakpoint: 500,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              arrows: true,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: true,
+            }
+          },
+        ]
+      });
+    }
+  
+    // Initialize the slider
+    var slider = initSlider();
+  
+    // Reinitialize the slider on window resize
+    $(window).on('resize', function() {
+      var windowWidth = $(window).width();
+  
+      if (windowWidth >= 767) {
+        // If the slider is initialized, destroy it
+        if (slider.hasClass('slick-initialized')) {
+          slider.slick('unslick');
+        }
+      } else {
+        // If the slider is not initialized, initialize it with the specific settings
+        if (!slider.hasClass('slick-initialized')) {
+          slider = initSlider();
+        }
+      }
+    });
+  
+
   });
+
 
 
 
